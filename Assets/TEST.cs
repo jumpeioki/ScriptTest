2144310c6cc9f500102bf621409ae86bf081f749
jumpeioki <joki@sun-denshi.co.jp>
2018-04-15 10:44:28 +0900

    Script TEST
    Lesson4の課題です

---
+++ Assets/TEST.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST : MonoBehaviour{
+
+	void Start(){
+		int[] array = { 10, 20, 30, 40, 50 };
+
+		for (int i = 4; i < array.Length; i-- );
+		{Debug.Log(array[i]);
+		}
+	}
+
+
+		
+
+	void Update () {
+		
+	}
+
+}
