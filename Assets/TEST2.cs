d2bce6a86b7a93303805e2da8a5b75382a4efaf7
jumpeioki <joki@sun-denshi.co.jp>
2018-04-19 06:56:54 +0900

    課題４です
    課題の修正と発展課題を追加しています。

---
+++ Assets/TEST2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST2 : MonoBehaviour {
+
+	public class Boss
+	{
+		int mp = 53;
+
+		public void Magic(int point){
+			mp -= point;
+			if (mp > 0) {
+				Debug.Log ("魔法攻撃をした.残りＭＰは" + mp);
+			}else
+			{Debug.Log ("MPが足りないため魔法が使えない");
+			}
+		}
+	}
+	void Start () {
+
+		Boss lastboss = new Boss ();
+
+		for (int i = 0; i <= 10; i ++) {
+			lastboss.Magic(5);}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TESTTEST.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTTEST : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		int[] array = { 10, 20, 30, 40, 50 };
+	for (int i = 4; i > -1; i--)
+		{
+			Debug.Log(i);
+		Debug.Log (array [i]);
+	}
+}
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
