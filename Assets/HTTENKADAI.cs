5a2477146caa7fa347b4edfd4b720449c286acb1
jumpeioki <joki@sun-denshi.co.jp>
2018-04-29 10:07:15 +0900

    課題4を再提出します

---
+++ Assets/HTTENKADAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {        
+
+	private int mp = 53;
+
+		public void Magic(){
+		mp -= 5;
+		if (mp >= 0) {
+			Debug.Log ("魔法攻撃をした.残りMPは"+ mp);
+		} else {Debug.Log ("MPが足りないため魔法が使えない");
+			
+		}
+	}
+}
+
+
+public class HTTENKADAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Boss lastaoss = new Boss ();
+		for(int i =1; i<11; i++)
+		{lastaoss.Magic ();}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/KADAI.cs
+++ Assets/KADAI.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TESTTEST : MonoBehaviour {
+public class KADAI : MonoBehaviour {
 
 
 	// Use this for initialization
 	void Start ()
 	{
 		int[] array = { 10, 20, 30, 40, 50 };
-	for (int i = 4; i > -1; i--)
+		for (int i = 0; i < 5; i++) 
 		{
-			Debug.Log(i);
+			Debug.Log (array [i]);
+		}
+		for (int i = 4; i > -1; i--)
+		{
 		Debug.Log (array [i]);
 	}
 }
